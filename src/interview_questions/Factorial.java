9ecb4e62063b4b7557f1e45cbf856ff8b7d0c273
pavan muppa <pawankumar.muppa@gmail.com>
2018-03-23 18:11:13 +0530

    New programm added to interview questions

---
+++ src/interview_questions/Factorial.java
@@ -0,0 +1,15 @@
+package interview_questions;
+
+public class Factorial 
+{
+	public static void main(String[] args) 
+	{
+		int number = 6;
+        long fact = 1;
+        for(int i = 1; i <= number; i++)
+        {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+	}
+}
