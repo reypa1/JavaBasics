831f2781272a83f1ded49210a66c0de5666077be
pavan muppa <pawankumar.muppa@gmail.com>
2018-03-27 19:08:23 +0530

    Flods triangle programm added to interview questions

---
+++ src/interview_questions/Floyds_triangle.java
@@ -0,0 +1,29 @@
+package interview_questions;
+
+import java.util.Scanner;
+
+public class Floyds_triangle 
+{
+public static void main(String[] args) 
+{
+	int n, num = 1, c, d;
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Enter the number of rows : ");
+    n = in.nextInt();
+
+    System.out.println("Floyd's triangle :-");
+
+    for ( c = 1 ; c <= n ; c++ )
+    {
+       for ( d = 1 ; d <= c ; d++ )
+       {
+          System.out.print(num+ " ");
+          num++;
+       }
+
+       System.out.println();
+    }
+	
+}
+}
